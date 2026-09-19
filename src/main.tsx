4d6afaf5c96c05a40874a4c76d7226b0da0fb1d8
anishhfgpl-uk <anishhfgpl@gmail.com>
2026-09-19 05:32:05 +0530

    feat: require login before school portal access

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
+import LoginGate from './components/LoginGate';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <LoginGate>
+      {(account, schoolProfile, logout) => <App account={account} schoolProfile={schoolProfile} onLogout={logout} />}
+    </LoginGate>
   </StrictMode>,
 );
